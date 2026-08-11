3278206a2809068325a1c16f32f6e6083a03d8dc
Bikash-Dushad <bikashghll@gmail.com>
2026-08-11 22:56:06 +0530

    env updated

--- src/config/bullmq.js
+++ src/config/bullmq.js
@@ -2,10 +2,10 @@ import { Queue } from "bullmq";
 import Redis from "ioredis";
 
 export const connection = new Redis({
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.REDISPORT,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
   maxRetriesPerRequest: null,
 });
 
